f1ef14740913afe3feba399193ec97ce0d556259
wkumler <wkumler@uw.edu>
2024-03-28 12:53:26 -0700

    Splitting modeling steps apart for ease of visualization

--- R/updateXcmsObjFeatsFunctions.R
+++ R/updateXcmsObjFeatsFunctions.R
@@ -1,20 +1,11 @@
 
-
-logModelFeatQuality <- function(peak_data, feature_labels, 
-                                log_formula=feat_class~med_cor+med_snr,
-                                likelihood_threshold=0.5, 
-                                verbosity=2){
+logModelFeatProb <- function(feature_metrics, feature_labels, 
+                             log_formula=feat_class~med_cor+med_snr,
+                             verbosity=2){
   # Todo: add checks for formula vars existing in feat_metrics
-  model_df <- peak_data %>%
-    group_by(feature) %>%
-    summarise(mzmed=unique(feat_mzmed), rtmed=unique(feat_rtmed)) %>%
-    left_join(feat_metrics, by="feature") %>%
-    mutate(feat_class=class_labels)
-  if(verbosity>1){
-    print(ggplot(model_df, aes(x=rtmed, y=mzmed, color=feat_class, label=feature)) + geom_point())
-    print(ggplot(model_df, aes(x=med_cor, y=med_snr, color=feat_class, label=feature)) + geom_point())
-    print(ggplot(model_df, aes(x=PC1, y=PC2, color=feat_class, label=feature)) + geom_point())
-  }
+  model_df <- feature_metrics %>%
+    left_join(data.frame(feat_class=feature_labels) %>% rownames_to_column("feature"),
+              by="feature")
   glmodel <- model_df %>%
     filter(!is.na(feat_class)) %>%
     mutate(feat_class=ifelse(feat_class=="Bad", 0, 1)) %>%
@@ -39,17 +30,39 @@ logModelFeatQuality <- function(peak_data, feature_labels,
     print(gp)
   }
   
-  keep_feats <- model_df %>%
-    mutate(pred=predict(glmodel, newdata=model_df, type = "response")) %>%
-    filter(pred>likelihood_threshold) %>%
+  preds <- predict(glmodel, newdata=model_df, type = "response")
+  names(preds) <- model_df$feature
+  preds
+}
+
+logModelFeatQuality <- function(feature_metrics, feature_labels, 
+                                log_formula=feat_class~med_cor+med_snr,
+                                likelihood_threshold=0.5, 
+                                verbosity=2){
+  feature_preds <- logModelFeatProb(feature_metrics, feature_labels, 
+                                    log_formula=log_formula,
+                                    verbosity=verbosity)
+  pred_df <- feature_metrics %>%
+    left_join(data.frame(feat_class=feature_labels) %>% rownames_to_column("feature"),
+              by="feature") %>%
+    left_join(data.frame(pred_prob=feature_preds) %>% rownames_to_column("feature"),
+              by="feature") %>%
+    mutate(pred_class=ifelse(pred_prob>likelihood_threshold, "Good", "Bad"))
+  
+  if(verbosity>0){
+    print(caret::confusionMatrix(factor(pred_df$pred_class), factor(pred_df$feat_class)))
+  }
+  
+  keep_feats <- pred_df %>%
+    filter(pred_class=="Good") %>%
     pull(feature)
 }
 
-updateXcmsObjFeats(xcms_obj, peak_data, feature_labels,
-                   likelihood_threshold=0.5, verbosity=2){
-  good_features <- logModelFeatQuality(peak_data, feature_labels, 
+updateXcmsObjFeats <- function(xcms_obj, feature_metrics, feature_labels,
+                               likelihood_threshold=0.5, verbosity=2){
+  good_features <- logModelFeatQuality(feature_metrics, feature_labels, 
                                        likelihood_threshold = likelihood_threshold, 
-                                       verbosity = verbosity)
+                                       verbosity = 2)
   featureDefinitions(xcms_obj) <- featureDefinitions(xcms_obj)[good_features,]
   xcms_obj
 }
